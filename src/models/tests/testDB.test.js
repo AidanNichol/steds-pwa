83affb91f012280ffd1ec5caf30d63a2ec057cb3
Aidan Nichol <aidan@nicholware.co.uk>
2020-03-23 10:05:03 +0000

    removed old tests

--- src/models/tests/testDB.test.js
+++
@@ -1,7 +0,0 @@
-import { db } from '../testDB.js';
-it('can access the couchdb ', async () => {
-  const item = await db.info();
-  expect(item.db_name).toBe('devbookings');
-  // expect(item.memNo).toBe(1049);
-  // expect(item.members.get('M1049').fullName).toBe('Aidan Nichol');
-});
