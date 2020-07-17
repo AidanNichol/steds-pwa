200bbc0b0fb99bdcc2c558b7b6906eb8b341938f
Aidan Nichol <aidan@nicholware.co.uk>
2020-07-17 18:57:17 +0100

    reformating changes

--- src/Components/help/PaymentHelp.jsx
+++ src/Components/help/PaymentHelp.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Icon } from '../utility/Icon';
 export const PaymentHelp = () => (
-  <div className="payments-help">
+  <div className='payments-help'>
     <dl>
       <dt>
-        <Icon type="P" /> Paid cash
+        <Icon type='P' /> Paid cash
       </dt>
       <dd>
         Use this option when the user has handed over cash or cheques to pay for walks.
@@ -14,28 +14,28 @@ export const PaymentHelp = () => (
         cash as change then just enter £32.
       </dd>
       <dt>
-        <Icon type="PX" /> Refund Payment
+        <Icon type='PX' /> Refund Payment
       </dt>
       <dd>
         Use this option where money has been handed over to the member in order to clear
         credits that the member may have.
       </dd>
       <dt>
-        <Icon type="T" /> Paid via Treasure
+        <Icon type='T' /> Paid via Treasure
       </dt>
       <dd>
         Use this option when payment has been made directly to the treasurer, usually via
         some form of bank transfer such as a bacs payment.
       </dd>
       <dt>
-        <Icon type="TX" /> Refund via Treasurer
+        <Icon type='TX' /> Refund via Treasurer
       </dt>
       <dd>
         Use this option when the treasurer has made refund for walks payments, e.g.
         credits have been refunded to the member by cheque or some form of bank transfer.
       </dd>
       <dt>
-        <Icon type="+" /> Add Credit
+        <Icon type='+' /> Add Credit
       </dt>
       <dd>
         This option is used to give the member credits when no new payments are recieved
@@ -43,7 +43,7 @@ export const PaymentHelp = () => (
         may also be necessary to use this to adjust for errors in this system.
       </dd>
       <dt>
-        <Icon type="+X" /> Remove Credit
+        <Icon type='+X' /> Remove Credit
       </dt>
       <dd>
         Used to remove the credits from the members account. e.g. in order to transfer
@@ -58,7 +58,7 @@ export const PaymentHelp = () => (
     <p>
       When taking payments a pot of cash and cheques is being accumulated to be handed
       over to the treasurer. The first two options in this list,{' '}
-      <Icon type="P" height="15px" /> & <Icon type="PX" height="15px" />, should be used
+      <Icon type='P' height='15px' /> & <Icon type='PX' height='15px' />, should be used
       if, and only if, it affects that pot of money. It should be possible to reconcile
       the pot of money with the sum of these transactions.
     </p>
